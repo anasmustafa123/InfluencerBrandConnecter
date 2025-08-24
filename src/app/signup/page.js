a449bbb4f78e2b2e3f6bb9debff459c480f86781
Your Name <anasmuostafa@gmail.com>
2025-08-24 20:58:27 +0300

    fix typo

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import SignUpForm from "./SignUpForm";
-S
+
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading signup form...</div>}>
