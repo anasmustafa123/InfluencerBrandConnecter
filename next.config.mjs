921bc59baea2f91b9cf26230f7e517d5f0ded1c2
Your Name <anasmuostafa@gmail.com>
2025-08-25 12:46:51 +0300

    fix tiktok verification url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/tiktok_verification',
+            destination: '/tiktokwYWNaTk2gMq0ZTsVRcTmU3sAxBqufInS.txt',
+          },
+        ]
+      },
+};
 
 export default nextConfig;
