5b21d4927c5af57fd7e8d3b517b72f51667937a1
Your Name <anasmuostafa@gmail.com>
2025-08-25 12:53:29 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: '/tiktok_verification',
-            destination: '/tiktokwYWNaTk2gMq0ZTsVRcTmU3sAxBqufInS.txt',
-          },
-        ]
-      },
-};
+const nextConfig = {};
 
 export default nextConfig;
