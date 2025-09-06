32b9b4db69b82a8ab1e1062d3a285cb09a2c5662
Your Name <anasmuostafa@gmail.com>
2025-09-06 16:55:16 +0300

    refresh on logout

--- src/app/api/user/logout/route.js
+++ src/app/api/user/logout/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  const res = NextResponse.redirect(new URL("/login", req.url));
+  const res = NextResponse.json({ success: true });  
   res.cookies.delete("token");
   console.log("Cookie deleted and redirecting to /login");
   return res;
