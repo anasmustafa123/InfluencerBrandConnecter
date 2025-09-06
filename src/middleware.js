2614076f4ebff990377697fa1fa2632dc90fb83e
Your Name <anasmuostafa@gmail.com>
2025-09-06 15:38:06 +0300

    add unprotected routes to middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,14 +1,20 @@
 import { NextResponse } from 'next/server'
 
-// const protectedRoutes = ['/packages', '/packageoverview', '/influencerprofile', '/brandprofile']
-const protectedRoutes = []
+const protectedRoutes = ['/packages', '/packageoverview', '/influencerprofile', '/brandprofile']
+const unprotectedRoutes = ['/signup', '/login']
 
 export function middleware(request) {
   const token = request.cookies.get('token')?.value 
+  console.log({middleware: token})
 
   if (protectedRoutes.some((path) => request.nextUrl.pathname.startsWith(path))) {
     if (!token) {
       return NextResponse.redirect(new URL('/login', request.url))
+    }
+  }else if (unprotectedRoutes.some((path) => request.nextUrl.pathname.startsWith(path))) {
+    if (token) {
+      console.log('home')
+      return NextResponse.redirect(new URL('/home', request.url))
     }
   }
 
